925f1070a65738c0f0697e5a8a2c7f3e53e5481a
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-06-24 23:53:45 +0300

    adding new tickets components & using multiple ng-content in button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { DashboardItemComponent } from "./dashboard/shared/dashboard-item/dashboard-item.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
