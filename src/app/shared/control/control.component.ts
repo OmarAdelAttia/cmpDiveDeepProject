da5fb60a96444c7bb0e3ae8d63c523981faee449
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-06-25 15:21:05 +0300

    practicing @HostListener & @HostBinding in control component

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, input } from '@angular/core';
+import { Component, HostBinding, HostListener, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,11 +8,22 @@ import { Component, ViewEncapsulation, input } from '@angular/core';
   styleUrl: './control.component.scss',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    // '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
 
+  // @HostBinding('class') className = 'control';
+
+  // @HostListener('click') onClick() {
+  //   console.log(`Welcome from Control Component`);
+  // }
+
   label = input.required<string>();
+
+  // onClick() {
+  //   console.log(`Welcome from Control Component`);
+  // }
 
 }
