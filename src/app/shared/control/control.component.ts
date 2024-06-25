f5d984cdfa0659cbe1e75fd6fe77cc69c797f5cf
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-06-25 12:55:24 +0300

    encapsulation & writing scss

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.scss'
+  styleUrl: './control.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
 
