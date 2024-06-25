781aae840208728efb5d8826d279d984d6d1ba19
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-06-25 22:38:37 +0300

    viewChild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.scss',
-  imports: [ButtonComponent, ControlComponent]
+  imports: [ButtonComponent, ControlComponent, FormsModule]
 })
 export class NewTicketComponent {
 
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild(ControlComponent) ctr?: ElementRef<ControlComponent>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  onSubmit(inputEl: HTMLInputElement, textareaVal: string) {
+    console.log(inputEl.value, textareaVal)
+    console.log(`Submit`);
+    this.form().nativeElement.reset();
+  }
 }
